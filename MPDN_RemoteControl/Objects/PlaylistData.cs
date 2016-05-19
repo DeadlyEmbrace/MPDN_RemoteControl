b974f3b965aeba118fe3f9a328311b116cc71232
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2016-05-19 19:14:00 +0200

    Update to use the new v3 RemoteControl API in the MPDN Extensions.

---
+++ MPDN_RemoteControl/Objects/PlaylistData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MPDN_RemoteControl.Objects
+{
+    public class PlaylistData
+    {
+        public string PlaylistName { get; set; }
+        public List<PlaylistItem> Playlist { get; set; }
+    }
+}
